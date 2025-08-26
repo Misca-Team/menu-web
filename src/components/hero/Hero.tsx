6403da72519413c23a11e7435df3b5c17ad7303a
Reza Ahmadi <r.ahmadi@rima.tax>
2025-08-26 11:57:59 +0330

    ♻️ cta and hero ui refactoring.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="bg-gradient-to-r from-orange-200 to-orange-400 text-center py-20">
-      <h1 className="text-4xl font-bold mb-4">
-        ساخت منو آنلاین رایگان برای رستوران و کافه
-      </h1>
-      <p className="text-lg mb-6">
-        با میسکا منوی خودتان را آنلاین کنید و مشتریان خود را بیشتر کنید
-      </p>
-      <button className="bg-black text-white px-6 py-3 rounded-lg">
-        شروع کنید
-      </button>
+    <section className="py-20">
+      <div className="flex max-w-[1320px] ml-auto mr-auto">
+        <div className="flex-1">
+          <h1 className="text-4xl font-bold mb-4">
+            راهی نو برای نمایش منوی دیجیتال شما!
+          </h1>
+          <p className="text-lg mb-6">
+            تجربه ای مدرن و بروز را به کاربران خود ارائه دهید. قابلیت مدیریت چندین کسب و کار، بهینه سازی برای گوگل و طراحی سازگار با انواع دستگاه ها بخشی از امکانات میسکا هست.
+          </p>
+          <div className="flex justify-end">
+          <Link href="/auth/signin" className="bg-purple-600 text-white px-6 py-3 rounded-lg">شروع رایگان</Link>
+          </div>
+        </div>
+        <div className="flex-1">
+          ss
+        </div>
+      </div>
     </section>
   );
 }
